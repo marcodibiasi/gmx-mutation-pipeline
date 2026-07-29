60468b34e5f4db66268db5f409847b1099f3efeb
marcodibiasi <dibiasimarco02@gmail.com>
2026-07-29 17:09:45 +0200

    updated run_simulation

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# gmx-mutation-pipeline
+# GROMACS Mutation Pipeline 🚧 (Work in Progress)
+
+*Note: This project is currently under active development.*
+
+This repository contains scripts to automate the preparation and simulation of molecular dynamics systems in GROMACS. 
+
+## 📁 Directory Structure
+
+* **`inputs/`** $\rightarrow$ Place your initial `.pdb` files here.
+* **`scripts/`** $\rightarrow$ Contains bash scripts (`run_pipeline.sh`).
+* **`simulations/`** $\rightarrow$ Output directory. The script generates isolated subfolders for each run.
+* **`analysis/`** $\rightarrow$ Reserved for post-simulation analysis.
+
+---
+
+## 🚀 Quick Start
+
+**1. Grant execution permissions (first time only):**
+```bash
+chmod +x scripts/run_pipeline.sh
+```
+
+**2. Run the setup script:**
+Pass the the path of the PDB file as an argument.
+```bash
+./scripts/run_pipeline.sh inputs/protein.pdb
+```
+
+## 📂 Generated Outputs
+
+For each execution, the script creates a dedicated subfolder inside `simulations/` (e.g., `simulations/run_protein/`). Inside this folder, you will find the prepared system and all the intermediate files:
+
+* **`*_box_ions.gro`**: The final, fully prepared structure containing the protein, water molecules, and ions. *(This is the main coordinate file for the next steps).*
+* **`*.top`**: The system topology file, automatically updated to include water and ions.
+* **`*.itp`**: Position restraint files used to lock the protein structure during equilibration.
+* **`logs/`**: A dedicated subdirectory containing all the raw text outputs from GROMACS commands (e.g., `pdb2gmx.log`, `solvate.log`). This keeps the workspace clean while allowing easy debugging if anything goes wrong.
--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -3,13 +3,13 @@
 # Exit on error
 set -e 
 
-# Check whether the first argument is empty 
+# CHECK WHETHER THE FIRST ARGUMENT IS EMPTY 
 if [ -z "$1" ]; then 
     echo "Error: no PDB file provided"
     exit 1 
 fi
 
-# Guard to avoid running the command in the root
+# GUARD TO ALLOW THE EXECUTION ONLY IN THE PROJECT ROOT DIRECTORY
 if [ ! -d "simulations" ] || [ ! -d "inputs" ]; then 
     echo "Error: run this script from the project root directory"
     exit 1
@@ -24,11 +24,104 @@ echo -e "\n>>> SIMULATION: $PROT"
 echo "[INFO] Workspace: $RUN_DIR"
 mkdir -p "$RUN_DIR"
 cd "$RUN_DIR" || exit 1
+mkdir -p "logs"
 
-# Topology
+
+# =======================
+#        TOPOLOGY
+# =======================
+# -f: input file (.pdb)
+# -i: position restrains (.itp) -> lock the protein structure
+# -o: output structure (.gro) -> spatial coordinates and velocities
+# -p: topology file (.top) -> system physical rules
+# -ff: forcefield
+# -water: water model
+# -ignh: ignore hydrogens
+# -heavyh: makes hydrogen atoms heavy -> reduce oscillations
+# -quiet: reduce the printed output
 echo "[1/4] Topology generation (pdb2gmx)..."
-gmx pdb2gmx -f "$ABS_PDB" -o "${PROT}_processed.gro" -p "${PROT}.top" -ff amber99sb-ildn -water tip3p -ignh -quiet &> pdb2gmx.log
+gmx pdb2gmx \
+    -f "$ABS_PDB" \
+    -i "${PROT}_posre.itp" \
+    -o "${PROT}_processed.gro" \
+    -p "${PROT}.top" \
+    -ff amber99sb-ildn \
+    -water tip3p \
+    -ignh \
+    -heavyh \
+    -quiet &> logs/pdb2gmx.log
 
-# Box 
+
+# =======================
+#          BOX
+# =======================
+# -f: input file (.gro)
+# -o: output box file (.gro)
+# -c: center molecule in box
+# -d: distance between the solute and the box
+# -bt: box type
+# -quiet: reduce the printed output
 echo "[2/4] Defining the simulation box (editconfig)..."
-gmx editconf -f "${PROT}_processed.gro" -o "${PROT}_box gro" -c -d 1.2 -bt cubic -quiet &> editconfig.log
+gmx editconf \
+    -f "${PROT}_processed.gro" \
+    -o "${PROT}_box.gro" \
+    -c \
+    -d 0.8 \
+    -bt cubic \
+    -quiet &> logs/editconfig.log
+
+
+# =======================
+#        SOLVATION
+# =======================
+# -cp: configuration protein (.gro): box containing the centered protein 
+# -cs: configuration solvent (.gro): generic water box (can be omitted)
+# -o: output structure (.gro): final coordinated of protein + water
+# -p: topology file (.top): auto-updates the topology file
+# -quiet: reduce the printed output
+echo "[3/4] Solvation of the system (solvate)..."
+gmx solvate \
+    -cp "${PROT}_box.gro" \
+    -cs spc216.gro \
+    -o "${PROT}_solvated.gro" \
+    -p "${PROT}.top" \
+    -quiet &> logs/solvate.log
+
+
+# =======================
+#      GENERATE IONS 
+# =======================
+# grompp (gromacs pre-processor):
+# -f: input parameters (.mdp): parameters for compilation
+# -c: input coordinates (.gro): solvated system
+# -p: topology file (.top)
+# -o: output run file (.trp): compiled binary file for genion
+# -quiet: reduce the printed output
+
+# genion (generate ions):
+# -s: input run file (.tpr)
+# -p: topology file (.top): auto-updates the topology file
+# -o: output structure (.gro): final system
+# -pname / -name: names of positive and negative ions 
+# -neutral: neutralize the system charge 
+# -conc: adds salt concentration
+# -quiet: reduce the printed output
+echo "[4/4] Adding ions to the system (grompp & genion)..."
+touch ions.mdp
+gmx grompp \
+    -f ions.mdp \
+    -c "${PROT}_solvated.gro" \
+    -p "${PROT}.top" \
+    -o ions.tpr \
+    -quiet &> logs/grompp.log
+
+echo "SOL" | gmx genion \
+    -s ions.tpr \
+    -p "${PROT}.top" \
+    -o "${PROT}_box_ions.gro" \
+    -pname NA \
+    -nname CL \
+    -neutral \
+    -conc 0.15 \
+    -quiet &> logs/genion.log
+
